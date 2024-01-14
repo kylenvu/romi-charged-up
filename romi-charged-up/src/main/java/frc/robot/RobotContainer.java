903e76dfad8f115eee35b68fb95b5b002d2c1efd
kylenvu <kylenvu@gmail.com>
2024-01-13 16:13:50 -0800

    implemented lambda commands

--- romi-charged-up/src/main/java/frc/robot/RobotContainer.java
+++ romi-charged-up/src/main/java/frc/robot/RobotContainer.java
@@ -5,9 +5,13 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.RomiDrivetrain;
-import edu.wpi.first.wpilibj2.command.Command;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -16,32 +20,61 @@ import edu.wpi.first.wpilibj2.command.Command;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final RomiDrivetrain m_romiDrivetrain = new RomiDrivetrain();
+    // The robot's subsystems and commands are defined here...
+    private final RomiDrivetrain m_drivetrain = new RomiDrivetrain();
+    private final ExampleCommand m_autoCommand = new ExampleCommand(m_drivetrain);
+    private final CommandXboxController m_controller = new CommandXboxController(1);
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_romiDrivetrain);
+    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    public RobotContainer() {
+        m_drivetrain.setDefaultCommand(this.getArcadeDriveCommand());
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the button bindings
-    configureButtonBindings();
-  }
+        // Configure the button bindings
+        configureButtonBindings();
+    }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {}
+    /**
+     * Use this method to define your button->command mappings. Buttons can be created by
+     * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its subclasses ({@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     */
+    private void configureButtonBindings() {
+        m_controller.b().onTrue(this.DriveUntilCommand(10));
+    }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
-  }
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        // An ExampleCommand will run in autonomous
+        return m_autoCommand;
+    }
+
+
+    public Command getArcadeDriveCommand() {
+        // return new ArcadeDrive(
+        //     m_drivetrain, () -> -m_controller.getRawAxis(1), () -> -m_controller.getRawAxis(2));
+        return new RunCommand(
+            () -> m_drivetrain.arcadeDrive(-m_controller.getLeftY(), m_controller.getRightX()),
+            m_drivetrain
+        );
+    }
+
+    public Command DriveUntilCommand(final double dist) {
+        return new FunctionalCommand(
+            // Reset encoders on command start
+            m_drivetrain::resetEncoders,
+            // Start driving forward at the start of the command
+            () -> m_drivetrain.arcadeDrive(0.5, 0),
+            // Stop driving at the end of the command
+            (interrupted) -> m_drivetrain.arcadeDrive(0, 0),
+            // End the command when the robot's driven distance exceeds the desired value
+            () -> m_drivetrain.getAverageEncoderDistance() >= dist,
+            // Require the drive subsystem
+            m_drivetrain
+        );
+    }
 }
